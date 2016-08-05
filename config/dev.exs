916bc3e771e5a4a07e7b13f220b4e73a4d7a23c7
Sergio Tapia <sergiotapiagutierrez@gmail.com>
2016-08-05 13:49:16 -0400

    Configure database credentials.

--- config/dev.exs
+++ config/dev.exs
@@ -36,8 +36,6 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :sass_example, SassExample.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "sass_example_dev",
   hostname: "localhost",
   pool_size: 10
